4b47d7c7b1db25694a80c2da725e5b385e412638
Stiv <strodriguezna@gmail.com>
2022-02-12 11:36:55 -0500

    beggin dynamization starts of the component Cabecero with the methods and variables proper of the same

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { IngresoServicio } from './ingreso/ingreso.servicio';
+import { Egreso } from './egreso/egreso.model';
+import { Ingreso } from './ingreso/ingreso.model';
 import { Component } from '@angular/core';
+import { EgresoServicio } from './egreso/egreso.servicio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+  ingersos: Ingreso []=[];
+  egresos: Egreso  []=[];
+
+  constructor(private ingresiServicio:IngresoServicio,
+              private egresoServicio:EgresoServicio){
+                this.ingersos = ingresiServicio.ingresos;
+                this.egresos = egresoServicio.egresos;
+              }
+
+  getIngresoTotal(){
+    let ingresoTotal:number = 0;
+    this.ingersos.forEach(ingreso =>{
+      ingresoTotal += ingreso.valor;
+    });
+    return ingresoTotal;
+  }
+
+  getEgresoTotal(){
+    let egresoTotal:number = 0;
+    this.egresos.forEach(egreso =>{
+      egresoTotal += egreso.valor;
+    });
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal(){
+    return this.getEgresoTotal()/this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal(){
+    return this.getIngresoTotal()-this.getEgresoTotal();
+  }
+
 }
---
+++ src/app/egreso/egreso.servicio.ts
@@ -0,0 +1,7 @@
+import { Egreso } from './egreso.model';
+export class EgresoServicio{
+  egresos:Egreso[] = [
+    new Egreso("Renta apartamento",900),
+    new Egreso("Ropa",200)
+  ];
+}
